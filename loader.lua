26d05df599590bfb8e15d8a9b80d0cb6b19a5c9c
AZYsGithub <114379782+AZYsGithub@users.noreply.github.com>
2023-02-27 23:38:07 +0800

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,13 @@
+--Anti skid
+if getgenv().ChillzAntiSkid123 ~= [[Chillz]] then
+    game.Players.LocalPlayer:Kick("Modify the script is allowed, but at least keep the original owner of the scipt >:(")
+elseif getgenv().ChillzAntiSkid1234 ~= [[AZY#0348]] then
+    game.Players.LocalPlayer:Kick("Modify the script is allowed, but at least keep the original owner of the scipt >:(")
+end
+--BlackListed
+if game.Players.LocalPlayer.Name == "HeroJayesh" then
+    game.Players.LocalPlayer:Kick("Seems you're got blacklisted, you may appeal in our server, discord.gg/VPn54EcfNX or contact AZY#0348, thank you.")
+end
+
+--AZY["94"]["Text"] = [[Chillz]];
+--AZY["cc"]["Text"] = [[AZY#0348]];
